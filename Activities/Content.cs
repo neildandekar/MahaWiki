f9be8237a0d66266e56005adf30ae1bc3181d8e2
Sunil Dandekar <Neil.Dandekar@outlook.com>
2020-03-28 12:22:25 +0530

    March282020

--- Activities/Content.cs
+++ Activities/Content.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Text.RegularExpressions;
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.V4.Text;
-using Android.Text;
 using Android.Views;
 using Android.Webkit;
+using Android.Views.InputMethods;
 using Android.Widget;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using LayoutParams = Android.Views.ViewGroup.LayoutParams;
+using System.Runtime.Remoting.Contexts;
 
 namespace MahaWiki.Activities
 {
@@ -24,11 +19,12 @@ namespace MahaWiki.Activities
         private Button butGet;
         private RadioButton rbMarathi;
         private RadioButton rbEnglish;
-        private ImageView ivImage;
+        private PopupWindow _popupImage;
         private bool isMarathi;
         private bool isEnglish;
         private EditText etSearch;
         private WebView htmlContent;
+        private ViewStub viewStub;
         private content cont = new content();
         private Dictionary<string,string> strCont = new Dictionary<string, string>();
         protected override void OnCreate(Bundle savedInstanceState)
@@ -38,14 +34,15 @@ namespace MahaWiki.Activities
             isEnglish = true;
             htmlContent = FindViewById<WebView>(Resource.Id.webView1);
             butGet = FindViewById<Button>(Resource.Id.butGet);
-            ivImage = FindViewById<ImageView>(Resource.Id.ivImage);
-            ivImage.SetImageResource(Resource.Drawable.Parat);
             rbMarathi = FindViewById<RadioButton>(Resource.Id.rbMarathi);
             rbEnglish = FindViewById<RadioButton>(Resource.Id.rbEnglish);
             etSearch = FindViewById<EditText>(Resource.Id.etSearch);
             butGet.Click += async delegate
             {
-                etSearch.ClearFocus();
+                InputMethodManager inputManager = (InputMethodManager)GetSystemService(InputMethodService);
+                inputManager.HideSoftInputFromWindow(etSearch.WindowToken, 0);   
+
+                if (etSearch.Text.ToString().Length < 1) { Toast.MakeText(this, "Please enter the search string", ToastLength.Short).Show(); return; }
                 HttpClient client = new HttpClient();
                 string url = null;
                 if (isMarathi)
@@ -86,54 +83,64 @@ namespace MahaWiki.Activities
             {
                 etSearch.Text = "";
             };
-             string convertToHTML(Dictionary<string,string> inDict)
+            string convertToHTML(Dictionary<string,string> inDict)
             {
                 string ph;
                 StringBuilder sb = new StringBuilder();
-                sb.Append("<body>");
-                sb.Append("<p>");
-                inDict.TryGetValue("uncKey", out ph);
-                sb.Append("<h1>");
-                inDict.TryGetValue("uncKey", out ph);
-                sb.Append(ph);
-                sb.Append("/");
-                inDict.TryGetValue("aka", out ph);
-                sb.Append(ph);
-                sb.Append("/");
-                inDict.TryGetValue("ansiKey", out ph);
-                sb.Append(ph);
-                sb.Append("</h1>");
-
-                sb.Append("<h2>या बाबतचे विवरण</h2><p>");
                 inDict.TryGetValue("description", out ph);
-                sb.Append(ph);
-                sb.Append("<p>");
+                if (ph != null && ph.Length > 0)
+                {
+                    sb.Append("<body>");
+                    sb.Append("<p>");
+                    inDict.TryGetValue("uncKey", out ph);
+                    sb.Append("<h1>");
+                    inDict.TryGetValue("uncKey", out ph);
+                    sb.Append(ph);
+                    sb.Append("/");
+                    inDict.TryGetValue("aka", out ph);
+                    sb.Append(ph);
+                    sb.Append("/");
+                    inDict.TryGetValue("ansiKey", out ph);
+                    sb.Append(ph);
+                    sb.Append("</h1>");
 
-                sb.Append("<h2>वर्गीकरण</h2><p>");
-                inDict.TryGetValue("category", out ph);
-                sb.Append(ph);
-                sb.Append("<p>");
+                    sb.Append("<h2>या बाबतचे विवरण</h2><p>");
+                    inDict.TryGetValue("description", out ph);
+                    sb.Append(ph);
+                    sb.Append("<p>");
 
-                sb.Append("<h2>अधिक माहिती</h2><p>");
-                inDict.TryGetValue("notes", out ph);
-                sb.Append(ph);
-                sb.Append("<p>");
+                    sb.Append("<h2>वर्गीकरण</h2><p>");
+                    inDict.TryGetValue("category", out ph);
+                    sb.Append(ph);
+                    sb.Append("<p>");
 
-                sb.Append("</body>");
+                    sb.Append("<h2>अधिक माहिती</h2><p>");
+                    inDict.TryGetValue("notes", out ph);
+                    sb.Append(ph);
+                    sb.Append("<p>");
 
-                sb.Append("<h2>हा शब्द या भागांत वापरला जातो</h2><p>");
-                inDict.TryGetValue("usage Region", out ph);
-                sb.Append(ph);
-                sb.Append("<p>");
+                    sb.Append("<h2>हा शब्द या भागांत वापरला जातो</h2><p>");
+                    inDict.TryGetValue("usage Region", out ph);
+                    sb.Append(ph);
+                    sb.Append("<p>");
 
-                sb.Append("<h2>हाही शब्द पहावा</h2><p>");
-                inDict.TryGetValue("aka", out ph);
-                sb.Append(ph);
-                sb.Append("<p>");
-
-                return sb.ToString();
+                    sb.Append("<h2>हाही शब्द पहावा</h2><p>");
+                    inDict.TryGetValue("aka", out ph);
+                    sb.Append(ph);
+                    sb.Append("<p>");
+                    sb.Append("</body>");
+                    return sb.ToString();
+                }
+                else
+                {
+                    sb.Append("<body>");
+                    sb.Append("<h1>या शब्दाचे विवरण सध्या उपलब्ध नाही</h1><p>");
+                    sb.Append("आपल्याला जर या शब्दाचे विवरण देण्यात स्वारस्य असेल तर, आपले नाव आणि गाव यासकट खालील पत्त्यावर संदेश पाठवावा. आपल्या नावाचा उल्लेख विवरणात केला जाईल.");
+                    sb.Append("या सामुदायिक प्रयासात आपला सहभाग आवश्यक आहे.<p>");
+                    sb.Append("neil.dandekar@outlook.com");
+                    return sb.ToString();
+                }
             }
-            // Create your application here, 
         }
     }
 }
